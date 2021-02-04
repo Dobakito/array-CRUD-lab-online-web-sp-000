474065973e36692a264679fc140adf91d48e070b
Michael Dobak <mdobak44@icloud.com>
2021-02-04 19:27:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array(array)
- array = [inzo, seven lions, sg lewis, flume]
+ array = [inzo, sevenlions, sglewis, flume]
 end
 
 def add_element_to_end_of_array(array, element)
